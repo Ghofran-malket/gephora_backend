ed86437e9483df73b37aca5d72e0b1cfbbd8243f
Ghofran <gh.malket@gmail.com>
2024-07-17 23:56:49 +0200

    Appliquer le composant « sérialisation » et rendre les méthodes de CRUD dynamiques

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -8,88 +8,86 @@ use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/store', name: 'app_api_store_')]
 class StoreController extends AbstractController
 {
 
-    public function __construct(private EntityManagerInterface $manager, private StoreRepository $repository)
-    {
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private StoreRepository $repository,
+        private SerializerInterface $serializer,
+        private UrlGeneratorInterface $urlGenerator
+    ) {
     }
 
-    #[Route('/', name:'create', methods:'POST')]
-    public function create(): Response{
-        // ... create le store dans la base de données
-        $store = new Store();
-        $store->setName('Gephora');
-        $store->setDescription('Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-         Lorem Ipsum has been the industrys standard dummy text ever since the 1500s');
-        $store->setEmail('gephora@store.fr');
-        $store->setAddress('Amiens 80000');
+    #[Route('/', name: 'create', methods: 'POST')]
+    public function create(Request $request): Response
+    {
+        
+        $store = $this->serializer->deserialize($request->getContent(), Store::class, 'json');
         $store->setCreatedAt(new DateTimeImmutable());
 
-        // Tell Doctrine you want to (eventually) save the store (no queries yet)
         $this->manager->persist($store);
-        // Actually executes the queries (i.e. the INSERT query)
         $this->manager->flush();
 
-        return $this->json(
-            ['message' => "Store resource created with {$store->getId()} id"],
-            Response::HTTP_CREATED,
+        $responseData = $this->serializer->serialize($store, 'json');
+        $location = $this->urlGenerator->generate(
+            'app_api_store_read',
+            ['id' => $store->getId()],
+            UrlGeneratorInterface::ABSOLUTE_URL,
         );
+        return new JsonResponse($responseData, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 
-    #[Route('/{id}', name:'read', methods:'GET')]
-    public function read(int $id): Response{
+    #[Route('/{id}', name: 'read', methods: 'GET')]
+    public function read(int $id): Response
+    {
         // ... read les informations du store
-        $store = $this->repository->findOneBy(['id'=> $id]);
-        if(!$store){
-            throw $this->createNotFoundException("No Store found for {$id} id");
+        $store = $this->repository->findOneBy(['id' => $id]);
+        if ($store) {
+            $responseData = $this->serializer->serialize($store, format:'json');
+            return new JsonResponse($responseData, Response::HTTP_OK, [], json:true);
         }
-        
-        return $this->json([
-            'message' => "A Store was found : {$store->getName()} for {$store->getId()} id"
-        ]);
-
+        return new JsonResponse(data:null, status: Response::HTTP_NO_CONTENT);
     }
 
     #[Route('/{id}', name: 'edit', methods: 'PUT')]
-    public function edit(int $id): Response
+    public function edit(int $id, Request $request): JsonResponse
     {
         // … Edite le store et le sauvegarde en base de données (par admin)
-        $store = $this->repository->findOneBy(['id'=> $id]);
-        if(!$store){
-            throw $this->createNotFoundException("No Store found for {$id} id");
+        $store = $this->repository->findOneBy(['id' => $id]);
+        if ($store) {
+            $store = $this->serializer->deserialize(
+                $request->getContent(),
+                Store::class,
+                'json',
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $store]
+            );
+            $store->setUpdatedAt(new DateTimeImmutable());
         }
-        $store->setName('Gephora 2');
-        $store->setDescription('Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-         Lorem Ipsum has been the industrys standard dummy text ever since the 1500s');
-        $store->setEmail('gephora@store.fr');
-        $store->setAddress('Amiens 80000');
-        $store->setUpdatedAt(new DateTimeImmutable());
 
-        // Actually executes the queries (i.e. the INSERT query)
         $this->manager->flush();
 
-        return $this->redirectToRoute('app_api_store_read', ['id' => $store->getId()]);
-
+        return new JsonResponse(data:null, status: Response::HTTP_NO_CONTENT);
     }
 
     #[Route('/{id}', name: 'delete', methods: 'DELETE')]
-    public function delete(int $id): Response
+    public function delete(int $id): JsonResponse
     {
         // ... Supprime le store de la base de données
-        $store = $this->repository->findOneBy(['id'=> $id]);
-        if(!$store){
-            throw $this->createNotFoundException("No Store found for {$id} id");
+        $store = $this->repository->findOneBy(['id' => $id]);
+        if (!$store) {
+            return new JsonResponse(data:null, status: Response::HTTP_NO_CONTENT);
         }
         $this->manager->remove($store);
         $this->manager->flush();
-        return $this->json([
-            'message' => "Restaurant resource deleted"],
-            Response::HTTP_NO_CONTENT
-        );
+        return new JsonResponse(data:null, status: Response::HTTP_NO_CONTENT);
     }
 }
